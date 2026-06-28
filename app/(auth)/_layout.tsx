1f3e057e943b313568729c9d2744db5ebe60468c
Yousef Na <yousefnasr0541@gmail.com>
2026-06-28 21:12:56 +0300

    feat: tab navigation

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,7 +1,6 @@
-import '@/global.css';
 import { Stack } from 'expo-router';
 import React from 'react';
 
-export default function RootLayout() {
+export default function AuthLayout() {
   return <Stack screenOptions={{ headerShown: false }} />;
 }
